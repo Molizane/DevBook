3a732bf16d21601aa037c6bf37d03b8e0cfef008
Molizane <molizane@gmail.com>
2023-04-03 18:57:40 -0300

    Alteração na linha de log
     Mudanças a serem submetidas:
    	modified:   api/src/middlewares/middlewares.go
    	modified:   webapp/src/middlewares/middlewares.go

--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -10,7 +10,7 @@ import (
 // Looger faz o log da requisição
 func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		log.Printf(": %s http://%s%s", r.Method, r.Host, r.RequestURI)
 		proximaFuncao(w, r)
 	}
 }
--- webapp/src/middlewares/middlewares.go
+++ webapp/src/middlewares/middlewares.go
@@ -9,7 +9,7 @@ import (
 // Logger escreve informações da requisição no terminal
 func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		log.Printf("%s http://%s%s", r.Method, r.Host, r.RequestURI)
 		proximaFuncao(w, r)
 	}
 }
