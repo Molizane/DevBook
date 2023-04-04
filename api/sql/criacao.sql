b41038fc3ca3021091c9b7b41ff7d04b9ac61613
Molizane <molizane@gmail.com>
2023-04-04 17:01:57 -0300

    Criando PK's e FK's com nomes explícitos

--- api/sql/criacao.sql
+++ api/sql/criacao.sql
@@ -14,12 +14,13 @@ DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios
 (
-        id int auto_increment primary key,
+        id int auto_increment,
         nome varchar(50) not null,
         nick varchar(50) not null unique,
         email varchar(50) not null unique,
         senha varchar(100) not null,
-        criadoEm timestamp DEFAULT current_timestamp() not null
+        criadoEm timestamp DEFAULT current_timestamp() not null,
+        CONSTRAINT pk_usuarios PRIMARY KEY (id)
 ) ENGINE = INNODB;
 
 CREATE TABLE seguidores
@@ -28,9 +29,9 @@ CREATE TABLE seguidores
         seguidor_id int not null,
         desde timestamp DEFAULT current_timestamp() not null,
         bloqueado tinyint DEFAULT 0 not null,
-        FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
-        FOREIGN KEY (seguidor_id) REFERENCES usuarios (id) ON DELETE CASCADE,
-        PRIMARY KEY (usuario_id, seguidor_id)
+        CONSTRAINT fk_seguidores_seguido FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
+        CONSTRAINT fk_seguidores_seguidor FOREIGN KEY (seguidor_id) REFERENCES usuarios (id) ON DELETE CASCADE,
+        CONSTRAINT pk_seguidores PRIMARY KEY (usuario_id, seguidor_id)
 ) ENGINE = INNODB;
 
 CREATE UNIQUE INDEX idx_seguidores_usuario_seguidor
@@ -38,13 +39,14 @@ ON seguidores (seguidor_id, usuario_id);
 
 CREATE TABLE publicacoes
 (
-        id int auto_increment not null primary key,
+        id int auto_increment not null,
         titulo varchar(100) not null unique,
         conteudo varchar(300) not null,
         autor_id int not null,
         curtidas int default 0 not null,
         criadaEm timestamp DEFAULT current_timestamp() not null,
-        FOREIGN KEY (autor_id) REFERENCES usuarios (id) ON DELETE CASCADE
+        CONSTRAINT fk_publicacoes_autor FOREIGN KEY (autor_id) REFERENCES usuarios (id) ON DELETE CASCADE,
+        CONSTRAINT pk_publicacoes PRIMARY KEY (id)
 ) ENGINE = INNODB;
 
 CREATE TABLE curtidas
@@ -52,9 +54,9 @@ CREATE TABLE curtidas
         publicacao_id int not null,
         usuario_id int not null,
         criadaEm timestamp DEFAULT current_timestamp() not null,
-        FOREIGN KEY (publicacao_id) REFERENCES publicacoes (id) ON DELETE CASCADE,
-        FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
-        PRIMARY KEY (publicacao_id, usuario_id)
+        CONSTRAINT fk_curtidas_publicacoes FOREIGN KEY (publicacao_id) REFERENCES publicacoes (id) ON DELETE CASCADE,
+        CONSTRAINT fk_curtidas_usuarios FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
+        CONSTRAINT pk_curtidas PRIMARY KEY (publicacao_id, usuario_id)
 ) ENGINE = INNODB;
 
 CREATE UNIQUE INDEX idx_curtidas_usuario_publicacao
@@ -65,9 +67,9 @@ CREATE TABLE descurtidas
         publicacao_id int not null,
         usuario_id int not null,
         criadaEm timestamp DEFAULT current_timestamp() not null,
-        FOREIGN KEY (publicacao_id) REFERENCES publicacoes (id) ON DELETE CASCADE,
-        FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
-        PRIMARY KEY (publicacao_id, usuario_id)
+        CONSTRAINT fk_descurtidas_publicacoes FOREIGN KEY (publicacao_id) REFERENCES publicacoes (id) ON DELETE CASCADE,
+        CONSTRAINT fk_descurtidas_usuarios FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE,
+        CONSTRAINT pk_descurtidas PRIMARY KEY (publicacao_id, usuario_id)
 ) ENGINE = INNODB;
 
 CREATE UNIQUE INDEX idx_descurtidas_usuario_publicacao
