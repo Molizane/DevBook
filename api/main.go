ec07fc06f43d3854ff055b4283301f70d506c331
Molizane <molizane@gmail.com>
2023-04-03 22:37:14 -0300

    Etc

--- api/main.go
+++ api/main.go
@@ -8,17 +8,19 @@ import (
 	"net/http"
 )
 
-// func init() {
-// 	chave := make([]byte, 64)
+/*
+func init() {
+	chave := make([]byte, 64)
 
-// 	if _, erro := rand.Read(chave); erro != nil{
-// 		log.Fatal(erro)
-// 	}
+	if _, erro := rand.Read(chave); erro != nil{
+		log.Fatal(erro)
+	}
 
-// 	stringBase64 := base64.StdEncoding.EncodeToString(chave)
+	stringBase64 := base64.StdEncoding.EncodeToString(chave)
 
-// 	fmt.Println(stringBase64)
-// }
+	fmt.Println(stringBase64)
+}
+*/
 
 func main() {
 	config.Carregar()
